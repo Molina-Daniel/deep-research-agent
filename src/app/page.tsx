274dcfa9f7edeb0e2320b6bd1a76f9ee16282741
Daniel Molina <danielmolina.dev@gmail.com>
2025-07-19 12:29:01 +0100

    feat: add topic input

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,20 @@
+import TopicInput from "@/components/TopicInput";
+
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-start min-h-screen w-full gap-8 py-16">
+    <main className="flex flex-col items-center justify-start min-h-screen w-full gap-8 py-16 px-4 text-center">
       <h1 className="text-4xl font-bold">
         Uncover Deep Insights with AI-Powered Research
       </h1>
-      <p className="text-lg text-center max-w-2xl text-muted-foreground">
+      <p className="text-lg max-w-2xl text-muted-foreground">
         An intelligent agent that asks the right follow-up questions, builds
         perfect search queries, and delivers structured, insightful research
         reports.
       </p>
+
+      <div className="w-full max-w-2xl">
+        <TopicInput />
+      </div>
     </main>
   );
 }
---
+++ src/components/TopicInput.tsx
@@ -0,0 +1,66 @@
+"use client";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form";
+
+const formSchema = z.object({
+  topicInput: z
+    .string()
+    .min(3, { message: "Topic must be at least 3 characters long" })
+    .max(50, { message: "Topic must be at most 50 characters long" }),
+});
+
+export default function TopicInput() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      topicInput: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values);
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="topicInput"
+          render={({ field }) => (
+            <FormItem>
+              <FormControl>
+                <Input
+                  className="text-center h-14 !text-lg sm:text-left"
+                  placeholder="What topic would you like to research?"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <div className="flex justify-center">
+          <Button
+            type="submit"
+            size="lg"
+            className="px-8 py-3 text-lg font-body font-medium cursor-pointer"
+          >
+            Begin
+          </Button>
+        </div>
+      </form>
+    </Form>
+  );
+}
